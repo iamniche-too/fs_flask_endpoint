9e58dfd39972cc644b32959c24231a8b18caaef1
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-07 14:37:13 +0000

    Fixed producer count for status Running.

--- scripts/get-producers-count.sh
+++ scripts/get-producers-count.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./export-gcp-credentials.sh
 
-kubectl -n producer-consumer get pods -o json --kubeconfig ./kubeconfig.yaml | jq '.items[].metadata.name | select (. | startswith("producer"))' | wc -w
+kubectl -n producer-consumer get pods --field-selector=status.phase==Running -o json --kubeconfig ./kubeconfig.yaml | jq '.items[].metadata.name | select (. | startswith("producer"))' | wc -w
