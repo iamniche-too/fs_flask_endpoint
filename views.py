a649c2f9526d4ac1af8e4e717c5880e9320663ec
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-05-04 15:04:17 +0100

    Added Burrow.

--- views.py
+++ views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import greenstalk
 
 import flask
+import requests
 from flask import Blueprint, render_template, request, make_response, jsonify
 
 views_blueprint = Blueprint('main_view', __name__)
@@ -11,28 +12,49 @@ views_blueprint = Blueprint('main_view', __name__)
 consumer_throughput_queue = greenstalk.Client(host='127.0.0.1', port=12000, use='consumer_throughput')
 
 current_filename = ""
-SCRIPT_DIR="./scripts"
+# this will be queried at run-time
+burrow_ip = None
+SCRIPT_DIR = "./scripts"
+BURROW_DIR = "../fs-burrow-k8s"
+
 
 def bash_command_with_output(additional_args, working_directory):
-  args = ['/bin/bash', '-e'] + additional_args
-  print(args)
-  p = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=working_directory)
-  p.wait()
-  out = p.communicate()[0].decode("UTF-8")
-  return out
+    args = ['/bin/bash', '-e'] + additional_args
+    print(args)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=working_directory)
+    p.wait()
+    out = p.communicate()[0].decode("UTF-8")
+    return out
+
+
+def set_burrow_ip():
+    global burrow_ip
+
+    filename = "./get-burrow-external-ip.sh"
+    args = [filename]
+
+    try:
+        burrow_ip = bash_command_with_output(args, BURROW_DIR)
+        print(f"burrow_ip={burrow_ip}")
+    except ValueError:
+        pass
+
+    return burrow_ip
+
 
 def get_producer_count():
-  filename = "./get-producers-count.sh"
-  args = [filename]
+    filename = "./get-producers-count.sh"
+    args = [filename]
 
-  producer_count = 0
-  try:
-    producer_count = int(bash_command_with_output(args, SCRIPT_DIR))
-  except ValueError:
-    pass
+    producer_count = 0
+    try:
+        producer_count = int(bash_command_with_output(args, SCRIPT_DIR))
+    except ValueError:
+        pass
 
-  print(f"reported_producer_count={producer_count}")
-  return producer_count
+    print(f"reported_producer_count={producer_count}")
+    return producer_count
+
 
 producer_id = 0
 
@@ -53,9 +75,25 @@ def producer_id_endpoint():
     return make_response(jsonify(success), 200)
 
 
+def get_consumer_lag(consumer_id):
+    global burrow_ip
+
+    burrow_cluster_name = "mykafka"
+
+    # see https://github.com/linkedin/Burrow/wiki/http-request-get-consumer-detail
+    if burrow_ip:
+        endpoint_url = f"http://{burrow_ip}/v3/kafka/{burrow_cluster_name}/consumer/{consumer_id}"
+        response = requests.get(endpoint_url)
+        print(response)
+    return 0
+
+
 @views_blueprint.route('/consumer_reporting_endpoint', methods=['POST'])
 def consumer_reporting_endpoint():
-    global current_filename
+    global current_filename, burrow_ip
+
+    if not burrow_ip:
+        set_burrow_ip()
 
     now = datetime.now()
 
@@ -79,9 +117,17 @@ def consumer_reporting_endpoint():
         file.write("\"values\": [\n")
         file.close()
     else:
-        print(f"Using current_filename={current_filename}")
+        print(f"Writing data to current_filename={current_filename}")
+
         producer_count = get_producer_count()
         data["producer_count"] = producer_count
+
+        if burrow_ip:
+            consumer_id = data["consumer_id"]
+            consumer_lag = get_consumer_lag(consumer_id)
+            data["consumer_lag"] = consumer_lag
+        else
+            print("Burrow IP not set so ignoring consumer lag...")
 
         # write out to file
         with open(current_filename, 'a') as outfile:
